06f0fc83fd3ed6a288b755028c76a88841dc7791
Leonardo Monaco <64544645+lnrdmnc@users.noreply.github.com>
2025-09-19 14:19:13 +0200

    fix add all

---
+++ test_files/c_out/test.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define BUFFER_SIZE 1024*4
+
+// Funzioni di supporto
+char* buffer;
+char* string_concat(char* s1, char* s2) {
+    char* ns = malloc(strlen(s1) + strlen(s2) + 1);
+    strcpy(ns, s1);
+    strcat(ns, s2);
+    return ns;
+}
+
+char* int2str(int n) {
+    char buffer[BUFFER_SIZE];
+    int len = sprintf(buffer, "%d", n);
+    char *ns = malloc(len + 1);
+    sprintf(ns, "%d", n);
+    return ns;
+}
+
+char* char2str(char c) {
+    char *ns = malloc(2);
+    sprintf(ns, "%c", c);
+    return ns;
+}
+
+char* float2str(float f) {
+    char buffer[BUFFER_SIZE];
+    int len = sprintf(buffer, "%f", f);
+    char *ns = malloc(len + 1);
+    sprintf(ns, "%f", f);
+    return ns;
+}
+
+char* bool2str(int b) {
+    char* ns = NULL;
+    if(b) {
+        ns = malloc(5);
+        strcpy(ns, "true");
+    } else {
+        ns = malloc(6);
+        strcpy(ns, "false");
+    }
+    return ns;
+}
+
+int main() {
+int x = 5;
+int y = 10;
+do{printf("%s\n", string_concat("stampa x: ", int2str(x)));
+printf("%s\n", string_concat("stampa y: ", int2str(y)));
+x = x + 1;
+y = y - 1;
+}
+while(x < y);
+
+    return 0;
+}
