ab751eb9b7d2333edbcfe3dc146e64b5bee6096d
Leonardo Monaco <64544645+lnrdmnc@users.noreply.github.com>
2025-10-03 19:26:45 +0200

    1 fase fatta aggiunto il tipo RGB, se controlli però appena il file.c nella dichiarazioni di più variabili RGB mette lo * solo alla prima es: char* primaryColor,secondaryColor;

---
+++ test_files/c_out/rgb.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#define BUFFER_SIZE 1024*4
+
+// Funzioni di supporto
+char* buffer;
+char* string_concat(char* s1, char* s2) {
+    char* ns = malloc(strlen(s1) + strlen(s2) + 1);
+    strcpy(ns, s1);
+    strcat(ns, s2);
+    return ns;
+}
+
+char* int2str(int n) {
+    char buffer[BUFFER_SIZE];
+    int len = sprintf(buffer, "%d", n);
+    char *ns = malloc(len + 1);
+    sprintf(ns, "%d", n);
+    return ns;
+}
+
+char* char2str(char c) {
+    char *ns = malloc(2);
+    sprintf(ns, "%c", c);
+    return ns;
+}
+
+char* float2str(float f) {
+    char buffer[BUFFER_SIZE];
+    int len = sprintf(buffer, "%f", f);
+    char *ns = malloc(len + 1);
+    sprintf(ns, "%f", f);
+    return ns;
+}
+
+char* bool2str(int b) {
+    char* ns = NULL;
+    if(b) {
+        ns = malloc(5);
+        strcpy(ns, "true");
+    } else {
+        ns = malloc(6);
+        strcpy(ns, "false");
+    }
+    return ns;
+}
+
+int main() {
+char* primaryColor,secondaryColor;
+
+primaryColor = "red";
+
+secondaryColor = "blue";
+
+if(strcmp(primaryColor, "red") == 0)printf("%s", "Il colore primario e' rosso");
+printf("%s\n", secondaryColor);
+
+    return 0;
+}
